b9a4047d06a0e2a1f7ba83e558601edd7edb8a7d
Sebastian Rojas <sebastianrojas812@gmail.com>
2023-10-17 14:48:13 -0400

    tweaks

--- app/api/[storeId]/checkout/route.ts
+++ app/api/[storeId]/checkout/route.ts
@@ -4,15 +4,13 @@ import { stripe } from "@/lib/stripe";
 import prismaDB from "@/lib/prismadb";
 
 const corsHeaders = {
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-    "Access-Control-Allow-Headers": "Content-Type, Authorization"
-}
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+};
 
 export async function OPTIONS() {
-    return NextResponse.json({}, {
-        headers: corsHeaders
-    });
+  return NextResponse.json({}, { headers: corsHeaders });
 }
 
 export async function POST(
